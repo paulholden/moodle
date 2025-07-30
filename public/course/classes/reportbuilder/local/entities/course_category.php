922424ccd2b52b8198cf3f4898664593dd60e120
Paul Holden <paulh@moodle.com>
2025-07-30 11:04:06 +0100

    MDL-86160 reportbuilder: consistently refer to aggregation types.
    Use existing API rather than hardcoded/non-discoverable strings.
    Improve test coverage of related functionality.

--- public/course/classes/reportbuilder/local/entities/course_category.php
+++ public/course/classes/reportbuilder/local/entities/course_category.php
@@ -25,10 +25,10 @@ use lang_string;
 use stdClass;
 use theme_config;
 use core_course_category;
+use core_reportbuilder\local\aggregation\{groupconcat, groupconcatdistinct};
 use core_reportbuilder\local\entities\base;
 use core_reportbuilder\local\filters\{category, select, text};
-use core_reportbuilder\local\report\column;
-use core_reportbuilder\local\report\filter;
+use core_reportbuilder\local\report\{column, filter};
 
 /**
  * Course category entity
@@ -150,7 +150,10 @@ class course_category extends base {
                 return empty($category->id) ? '' :
                     core_course_category::get($category->id, MUST_EXIST, true)->get_nested_name(false);
             })
-            ->set_disabled_aggregation(['groupconcat', 'groupconcatdistinct'])
+            ->set_disabled_aggregation([
+                groupconcat::get_class_name(),
+                groupconcatdistinct::get_class_name(),
+            ])
             ->set_is_sortable(true);
 
         // ID number column.
