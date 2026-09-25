775ff9f16e6e418509430a7fe48765bec9063787
Paul Holden <paulh@moodle.com>
2026-09-25 11:02:00 +0100

    MDL-84712 reportbuilder: final removal of deprecated renderer method.

--- public/reportbuilder/classes/output/renderer.php
+++ public/reportbuilder/classes/output/renderer.php
@@ -34,7 +34,6 @@ use core_reportbuilder\local\models\report;
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class renderer extends plugin_renderer_base {
-
     /**
      * Render a system report
      *
@@ -105,20 +104,16 @@ class renderer extends plugin_renderer_base {
     }
 
     /**
-     * Renders the New report button
-     *
-     * @return string
-     *
      * @deprecated since Moodle 5.0 - please use {@see \core_reportbuilder\system_report::set_report_action} instead
      */
-    #[\core\attribute\deprecated('\core_reportbuilder\system_report::set_report_action', mdl: 'MDL-82936', since: '5.0')]
+    #[\core\attribute\deprecated(
+        '\core_reportbuilder\system_report::set_report_action',
+        mdl: 'MDL-82936',
+        since: '5.0',
+        final: true,
+    )]
     public function render_new_report_button(): string {
         \core\deprecation::emit_deprecation([self::class, __FUNCTION__]);
-
-        return html_writer::tag('button', get_string('newreport', 'core_reportbuilder'), [
-            'class' => 'btn btn-primary my-auto',
-            'data-action' => 'report-create',
-        ]);
     }
 
     /**
@@ -132,12 +127,12 @@ class renderer extends plugin_renderer_base {
         $editdetailsbutton = html_writer::tag('button', get_string('editdetails', 'core_reportbuilder'), [
             'class' => 'btn btn-outline-secondary me-2',
             'data-action' => 'report-edit',
-            'data-report-id' => $report->get('id')
+            'data-report-id' => $report->get('id'),
         ]);
         $closebutton = html_writer::link(new moodle_url('/reportbuilder/index.php'), get_string('close', 'core_reportbuilder'), [
             'class' => 'btn btn-secondary',
             'title' => get_string('closeeditor', 'core_reportbuilder', $reportname),
-            'role' => 'button'
+            'role' => 'button',
         ]);
         $context = [
             'title' => $reportname,
